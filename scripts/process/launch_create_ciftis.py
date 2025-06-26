9b2ba8382191310ace8cb0c93a4c5d1511738a5d
Ellyn Butler <ellynrbutler@gmail.com>
2025-06-26 14:44:26 -0500

    fix launch script

--- scripts/process/launch_create_ciftis.py
+++ scripts/process/launch_create_ciftis.py
@@ -21,17 +21,19 @@ if not os.path.exists(outdir):
 if not os.path.exists(launchdir):
     os.mkdir(launchdir)
 
-subdirs = glob.glob(indir + "sub-*")
+subdirs = glob.glob(indir + "sub-*[0-9]")
 
 for subdir in subdirs:
     sub = subdir.split('/')[9]
     if not os.path.exists(outdir+sub):
         os.mkdir(outdir+sub)
-    sub_bold_imgs = glob.glob(indir+sub+'/*/*bold.nii.gz')
-    sessions = np.unique([i.split('/')[10] for i in sub_bold_imgs])
-    for ses in sessions:
+    sessions = glob.glob(indir+sub+'/ses-*')
+    sessions = [d for d in sessions if any(os.path.isdir(os.path.join(d, f)) for f in os.listdir(d))]
+    for ses_path in sessions:
+        ses = ses_path.split('/')[10]
         if not os.path.exists(outdir+sub+'/'+ses):
             os.mkdir(outdir+sub+'/'+ses)
+    sub_bold_imgs = glob.glob(indir+sub+'/ses-*/func/*_space-T1w_desc-preproc_bold.nii.gz')
     if len(sub_bold_imgs) > 0:
         cmd = ['bash /projects/b1108/studies/rise/scripts/process/create_ciftis.sh -s', sub]
         create_ciftis_script = launchdir+sub+'_create_ciftis_run.sh'
