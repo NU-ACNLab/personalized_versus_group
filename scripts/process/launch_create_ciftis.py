0b6219c6cc6f3684e4cc2dd25bb761cc96798d65
Ellyn Butler <ellynrbutler@gmail.com>
2025-06-26 14:29:33 -0500

    get ready for surfaces

---
+++ scripts/process/launch_create_ciftis.py
@@ -0,0 +1,41 @@
+### This script creates output directories
+###
+### Ellyn Butler
+### June 24, 2025 - June 26, 2025
+
+
+import os
+import shutil
+import re
+import numpy as np
+import glob
+import nibabel as nib
+
+indir = '/projects/b1108/studies/rise/data/processed/neuroimaging/fmriprep_23.2.0/'
+outdir = '/projects/b1108/projects/personalized_versus_group/data/processed/neuroimaging/surf/'
+launchdir = '/projects/b1108/projects/personalized_versus_group/data/processed/neuroimaging/launch/surf/'
+
+if not os.path.exists(outdir):
+    os.mkdir(outdir)
+
+if not os.path.exists(launchdir):
+    os.mkdir(launchdir)
+
+subdirs = glob.glob(indir + "sub-*")
+
+for subdir in subdirs:
+    sub = subdir.split('/')[9]
+    if not os.path.exists(outdir+sub):
+        os.mkdir(outdir+sub)
+    sub_bold_imgs = glob.glob(indir+sub+'/*/*bold.nii.gz')
+    sessions = np.unique([i.split('/')[10] for i in sub_bold_imgs])
+    for ses in sessions:
+        if not os.path.exists(outdir+sub+'/'+ses):
+            os.mkdir(outdir+sub+'/'+ses)
+    if len(sub_bold_imgs) > 0:
+        cmd = ['bash /projects/b1108/studies/rise/scripts/process/create_ciftis.sh -s', sub]
+        create_ciftis_script = launchdir+sub+'_create_ciftis_run.sh'
+        os.system('cat /projects/b1108/studies/rise/scripts/process/sbatchinfo_9hr_10G_general.sh > '+create_ciftis_script)
+        os.system('echo '+' '.join(cmd)+' >> '+create_ciftis_script)
+        os.system('chmod +x '+create_ciftis_script)
+        os.system('sbatch -o '+launchdir+sub+'.txt'+' '+create_ciftis_script)
