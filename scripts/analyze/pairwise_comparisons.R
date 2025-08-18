734c44fab1cf72051f87cc2d79838b237d17732e
Ellyn Butler <ellynrbutler@gmail.com>
2025-08-17 20:36:02 -0500

    within network poster analyses

--- scripts/analyze/pairwise_comparisons.R
+++ scripts/analyze/pairwise_comparisons.R
@@ -6,6 +6,65 @@
 
 # Load libraries
 library(ggplot2)
+library(psych)
 
 # Load data
 df <- read.csv('~/Documents/Northwestern/projects/personalized_versus_group/data/processed/combined/poster_2025-08-17.csv')
+df <- df[, c('subid', 'sesid', 'rrs_sum', 'bdi_sum', 'punishment', 'reward', 
+             names(df)[grepl('^(?:[^_]*_){3}[^_]*$', names(df))])]
+
+# A. Group versus personalized
+gp_df <- data.frame(clins = rep(c('rrs_sum', 'bdi_sum', 'punishment', 'reward'), 17),
+                    nets = c(rep('visuala', 4), rep('visualb', 4), rep('somatomotora', 4),
+                             rep('somatomotorb', 4), rep('dorsalattentiona', 4),
+                             rep('dorsalattentionb', 4), rep('saliencea', 4),
+                             rep('salienceb', 4), rep('limbica', 4), rep('limbicb', 4),
+                             rep('controla', 4), rep('controlb', 4), rep('controlc', 4), 
+                             rep('defaulta', 4), rep('defaultb', 4), rep('defaultc', 4), 
+                             rep('temporalparietal', 4)),
+                    r12 = NA, r13 = NA, r23 = NA, t = NA, p = NA)
+for (i in 1:nrow(gp_df)) {
+    clin <- gp_df[i, 'clins']
+    net <- gp_df[i, 'nets']
+    gname <- paste0('FC_group_', net, '_pos')
+    pname <- paste0('FC_pers_', net, '_pos')
+    r12 <- cor(df[, gname], df[, clin])
+    r13 <- cor(df[, pname], df[, clin])
+    r23 <- cor(df[, gname], df[, pname])
+    gp_df[i, 'r12'] <- r12
+    gp_df[i, 'r13'] <- r13
+    gp_df[i, 'r23'] <- r23
+    res <- r.test(n = nrow(df), r12 = r12, r13 = r13, r23 = r23)
+    gp_df[i, 't'] <- res$t
+    gp_df[i, 'p'] <- res$p
+}
+gp_df$p_fdr <- p.adjust(gp_df$p, method = 'fdr')
+gp_df[gp_df$p_fdr < .05, ] #none significant
+
+# B. Personalized versus intersection
+pi_df <- data.frame(clins = rep(c('rrs_sum', 'bdi_sum', 'punishment', 'reward'), 17),
+                    nets = c(rep('visuala', 4), rep('visualb', 4), rep('somatomotora', 4),
+                             rep('somatomotorb', 4), rep('dorsalattentiona', 4),
+                             rep('dorsalattentionb', 4), rep('saliencea', 4),
+                             rep('salienceb', 4), rep('limbica', 4), rep('limbicb', 4),
+                             rep('controla', 4), rep('controlb', 4), rep('controlc', 4), 
+                             rep('defaulta', 4), rep('defaultb', 4), rep('defaultc', 4), 
+                             rep('temporalparietal', 4)),
+                    r12 = NA, r13 = NA, r23 = NA, t = NA, p = NA)
+for (i in 1:nrow(pi_df)) {
+    clin <- pi_df[i, 'clins']
+    net <- pi_df[i, 'nets']
+    pname <- paste0('FC_pers_', net, '_pos')
+    iname <- paste0('FC_int_', net, '_pos')
+    r12 <- cor(df[, pname], df[, clin])
+    r13 <- cor(df[, iname], df[, clin])
+    r23 <- cor(df[, pname], df[, iname])
+    pi_df[i, 'r12'] <- r12
+    pi_df[i, 'r13'] <- r13
+    pi_df[i, 'r23'] <- r23
+    res <- r.test(n = nrow(df), r12 = r12, r13 = r13, r23 = r23)
+    pi_df[i, 't'] <- res$t
+    pi_df[i, 'p'] <- res$p
+}
+pi_df$p_fdr <- p.adjust(pi_df$p, method = 'fdr')
+pi_df[pi_df$p_fdr < .05, ] #none significant
--- scripts/clean/create_combined_data_z1.R
+++ scripts/clean/create_combined_data_z1.R
@@ -9,16 +9,22 @@ library(tidyverse)
 # Load data
 clin_df <- read.csv('~/Documents/Northwestern/projects/personalized_versus_group/data/processed/clinical/clinical_2025-08-11.csv')
 demo_df <- read.csv('~/Documents/Northwestern/projects/personalized_versus_group/data/processed/demographic/demographic_2025-08-11.csv')
-net_df <- read.csv('~/Documents/Northwestern/projects/personalized_versus_group/data/processed/neuroimaging/tabulated/surf_network_metrics_z1_2025-08-17.csv')
+net_df <- read.csv('~/Documents/Northwestern/projects/personalized_versus_group/data/processed/neuroimaging/tabulated/surf_network_metrics_z1_2025-08-17.csv') # complete cases 386 here
 
 # Merge
-df <- list(clin_df, demo_df, net_df) %>% 
-        reduce(left_join, by = c('subid', 'sesid'))
-dim(df) # N = 391
+df <- merge(clin_df, demo_df)
+df <- merge(df, net_df) 
+dim(df) # N = 335
+
+# Brain missing clinical?
+net_df$subid[!(net_df$subid %in% clin_df$subid)] # 52 subjects with brain data do not have clinical data
+
+# Brain missing demographic?
+net_df$subid[!(net_df$subid %in% demo_df$subid)] # 2 subjects with brain data do not have demographic data (part of 52 above)
 
 # Get a sense of missing data
-df2 <- df[complete.cases(df), ] 
-dim(df2) # N = 329
+df2 <- df[complete.cases(df[, c(1:8, 12:ncol(df))]), ] #don't care if no gender, race, latino
+dim(df2) # N = 334
 
 # Write out poster data
-write.csv(paste0('~/Documents/Northwestern/projects/personalized_versus_group/data/processed/combined/poster_', format(Sys.Date(), "%Y-%m-%d"), '.csv'), row.names = FALSE)
+write.csv(df2, paste0('~/Documents/Northwestern/projects/personalized_versus_group/data/processed/combined/poster_', format(Sys.Date(), "%Y-%m-%d"), '.csv'), row.names = FALSE)
