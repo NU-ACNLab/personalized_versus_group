c32b6b6b65b1fbb912bef839e2321dda4e4c7cab
Ellyn Butler <ellynrbutler@gmail.com>
2025-09-19 15:21:48 -0500

    determine prior subjects

--- scripts/process/determine_prior_subjects.R
+++ scripts/process/determine_prior_subjects.R
@@ -4,30 +4,35 @@
 ### for use in estimating the prior
 ###
 ### Ellyn Butler
-### July 8, 2025 - September 18, 2025
+### July 8, 2025 - September 19, 2025
 
-indir <- '/projects/b1108/projects/personalized_versus_group/data/processed/neuroimaging/'
+# Load data
+indir <- '~/Documents/Northwestern/projects/personalized_versus_group/data/processed/'
 
-qual_df <- read.csv(paste0(indir, 'tabulated/quality_2025-09-18.csv'))
-fd_df <- read.csv(paste0(indir, 'tabulated/meanFD_2025-09-18.csv'))
-clin_df <- 
-
-# Select for Chatroom and session 1
-df <- df[df$sesid == 1 & df$taskrun == 'task-chatroom_run-01', ]
-# Now: N = 390
+qual_df <- read.csv(paste0(indir, 'neuroimaging/tabulated/quality_2025-09-18.csv'))
+qual_df <- qual_df[qual_df$sesid == 1, ]
+fd_df <- read.csv(paste0(indir, 'neuroimaging/tabulated/meanFD_2025-09-18.csv')) # N = 402
+clin_df <- read.csv(paste0(indir, 'clinical/clinical_2025-08-11.csv')) # N = 381
 
 # Calculate the number of minutes remaining
-df$minutes <- ((df$nTRs - df$nRegressors)*2.05)/60
+qual_df$minutes <- ((qual_df$nTRs - qual_df$nRegressors)*2.05)/60
 
-# Number of subjects with fewer than 5 minutes remaining? 3
-sum(df$minutes < 5)
+# Merge fd and clin
+df <- merge(fd_df, clin_df) # N = 349
 
-# Filter out these subjects
-df <- df[df$minutes > 5,]
-# Now: N = 387
+# Sum the number of minutes
+df$minutes <- NA
+for (i in 1:nrow(df)) {
+    df[i, 'minutes'] <- sum(qual_df[qual_df$subid == df[i, 'subid'], 'minutes']) 
+}
 
-# Filter for only the subid and sesid columns
-df <- df[, c('subid', 'sesid')]
+# Filter out subjects with fewer than 5 minutes remaining
+sum(df$minutes < 5) # number of these subjects = 0
+# range(df$minutes)... 8.575833 29.041667
+#df <- df[df$minutes > 5,]
+
+# Filter out subjects with meanFD > .5
+df <- df[df$meanFD < .5, ] # N = 324
 
 # Write csv
-write.csv(df, paste0(indir, 'tabulated/prior_subjects.csv'), row.names = FALSE)
+write.csv(df, paste0(indir, 'neuroimaging/tabulated/prior_subjects_', format(Sys.Date(), "%Y-%m-%d"), '.csv'), row.names = FALSE)
