23eb2850d9dbeed8411cdf35b72483f03a72d289
Ellyn Butler <ellynrbutler@gmail.com>
2025-10-03 09:54:14 -0500

    SVD complete

--- scripts/process/get_components.R
+++ scripts/process/get_components.R
@@ -2,9 +2,12 @@
 ### engagement maps via singular value decomposition
 ###
 ### Ellyn Butler
-### October 2, 2025
+### October 2, 2025 - 
+
+set.seed(1432)
 
 # Load libraries
+library(ggplot2)
 library(ciftiTools)
 ciftiTools.setOption('wb_path', '/projects/b1108/software/workbench')
 
@@ -35,20 +38,59 @@ xxtransmat <- tcrossprod(xmat)
 
 # U (# of subjects by # of components)
 #Note: U is orthogonal
+#Note: # of components = # of subjects here
 svd_out <- svd(xxtransmat)
 umat <- svd_out$u #correct dimensions
 
-# D (# of components by # of vertices)
+# D (# of components by # of subjects)
 A <- t(umat) %*% xxtransmat %*% umat
-dmat <- diag(sqrt(diag(A))) #QUESTION: Getting an NA here... What is that about?
+dmat <- diag(sqrt(diag(A))) #QUESTION: Getting an NA here... What is that about? Result of centering... but we still want to center?
 
-# V' (# of vertices by # of vertices)
-vtransmat <- 
+# V' (# of subjects by # of vertices) #QUESTION: How is this useful for visualizing components? Do I have the axes wrong?
+vtransmat <- solve(dmat) %*% t(umat) %*% xmat
 
-# Singular values
-singvals <- svd_out$d
+# Total variance
+totvar <- sum(dmat[!is.na(dmat)]^2)
 
+# Variance explained by each components (same as singular values, svd_out$d)
+ddiag <- diag(dmat)
+dsq <- ddiag^2
+varex <- dsq/totvar
+varex <- varex[!is.na(varex)]
 
+df <- data.frame(component = 1:length(varex),
+                 variance_explained = varex,
+                 cumulative_variance_explained = cumsum(varex))
+
+write.csv(df, paste0(indir, 'tabulated/variance_explained_svd.csv'), row.names = FALSE)
+
+plot1 <- ggplot(df, aes(x = component, y = variance_explained)) +
+            theme_linedraw() + 
+            geom_line() + 
+            geom_point(size = 4) +
+            xlab('Component') + 
+            ylab('Variance Explained') +
+            ylim(0, .03)
+
+pdf('/projects/b1108/projects/personalized_versus_group/plots/variance_explained_svd.pdf', width = 8, height = 6) 
+plot1
+dev.off()
+
+plot2 <- ggplot(df, aes(x = component, y = cumulative_variance_explained)) +
+            theme_linedraw() + 
+            geom_line() + 
+            geom_point(size = 4) +
+            xlab('Component') + 
+            ylab('Cumulative Variance Explained') +
+            ylim(0, 1)
+
+pdf('/projects/b1108/projects/personalized_versus_group/plots/cumulative_variance_explained_svd.pdf', width = 8, height = 6) 
+plot2
+dev.off()
+
+# Save workspace for future tinkering (delete/modify later)
+save.image('/projects/b1108/projects/personalized_versus_group/data/processed/neuroimaging/tmp/svd.RData')
+#load('/projects/b1108/projects/personalized_versus_group/data/processed/neuroimaging/tmp/svd.RData')
 
 
 # svd() notes
