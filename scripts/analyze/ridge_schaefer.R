0a8c9e25eb32cd346fa2a8e558912742be2a0d5e
Ellyn Butler <ellynrbutler@gmail.com>
2025-12-24 23:49:53 -0600

    more ridge

--- scripts/analyze/ridge_schaefer.R
+++ scripts/analyze/ridge_schaefer.R
@@ -5,28 +5,62 @@
 ### Ellyn Butler
 ### December 24, 2025
 
+library(glmnet)
+
+indir <- '/projects/b1108/projects/personalized_versus_group/data/processed/neuroimaging/tabulated/'
+#comp_df <- read.csv(paste0(indir, 'spatial_components_1perc_2025-12-14.csv'))
+clin_df <- read.csv(paste0(indir, 'prior_subjects_2025-09-19.csv'))
+net_df <- read.csv(paste0(indir, 'schaefer_400_2025-12-24.csv'))
+
+df <- merge(clin_df, net_df)
+
+# Select variables to include in model
+FCs <- names(df)[grepl('FC', names(df))]
+
+# Regression
+clins <- c('rrs_sum', 'bdi_sum', 'punishment', 'reward')
+
 lambda_seq <- 10^seq(3, -3, by = -.1)
+X <- scale(as.matrix(df[, FCs]))
 
-X <- scale(as.matrix(df[, c(FCs, exps, comps)]))
+for (clin in clins) {
         y <- scale(as.matrix(df[, clin]))
-        penalized_columns <- c(rep(0, length(FCs)), rep(1, length(exps)), rep(1, length(comps)))
+        #penalized_columns <- c(rep(0, length(FCs)), rep(1, length(exps)), rep(1, length(comps)))
+        
+        ind <- sample(1:324)
 
+        lst <- split(ind, cut(seq_along(ind), 5, labels = FALSE))
+        full_rsqs <- c()
+        for (i in 1:5) {
+            X_train <- X[unlist(lst[(1:5)[-i]], use.names = FALSE), ]
+            y_train <- y[unlist(lst[(1:5)[-i]], use.names = FALSE), ]
+            X_test <- X[lst[[i]], ]
+            y_test <- y[lst[[i]]]
+            # Full model
+            cv_ridge <- cv.glmnet(X_train, y_train, alpha = 0, lambda = lambda_seq)
+                        #penalty.factor = penalized_columns)
+            best_lambda <- cv_ridge$lambda.min # rrs_sum chose 1000, 
+            print(paste0(clin, ' ', best_lambda))
 
-# Full model
-        cv_ridge <- cv.glmnet(X, y, alpha = 0, lambda = lambda_seq,
-                    penalty.factor = penalized_columns)
-        best_lambda <- cv_ridge$lambda.min #NOTE: Keeps picking the biggest one, even when expanded range of seq
+            ridge_model <- cv_ridge$glmnet.fit
+            #coef(ridge_model, s = best_lambda)
+            #head(ridge_model)
 
-        ridge_model <- cv_ridge$glmnet.fit
-        coef(ridge_model, s = best_lambda)
-        head(ridge_model)
+            # Make predictions with the best model (replace s = best_lambda with the appropriate value)
+            y_predicted <- predict(ridge_model, s = best_lambda, newx = X_test) 
+            
+             # Calculate SST
+            sst <- sum((y_ordered - mean(y_ordered))^2)
 
-        # Make predictions with the best model (replace s = best_lambda with the appropriate value)
-        y_predicted <- predict(ridge_model, s = best_lambda, newx = X) #Can I use the old X here?
+            # Calculate SSE
+            sse <- sum((y_predicted - y_ordered)^2)
 
-        # Calculate SST
-        sst <- sum((y - mean(y))^2)
-
-        # Calculate SSE
-        sse <- sum((y_predicted - y)^2)
-        full_rsq <- 1 - (sse/sst)
+            # Get rsq
+            full_rsq <- 1 - (sse/sst)
+            full_rsqs <- c(full_rqs, full_rsq)
+        }
+        assign(paste0('full_rsqs_', clin))
+        print(paste0('A ridge model with ', clin, 
+            ' as the outcome measure has an average out of sample R squared value of ', 
+            mean(full_rsq)))
+}
